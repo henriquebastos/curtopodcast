61e3d902441e50f391662d59b8d1057b148eb20b
Henrique Bastos <henrique@bastos.net>
2013-08-17 10:37:49 -0300

    Install django-suit

--- curtocircuito/settings.py
+++ curtocircuito/settings.py
@@ -143,7 +143,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
+    'suit',
     'django.contrib.admin',
     'cms',
     'mptt',
