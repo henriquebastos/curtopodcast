cf2a51cf1c2aecde47a997978d62b1607036d3a8
Henrique Bastos <henrique@bastos.net>
2013-09-03 23:14:09 -0300

    Remove i18n for urlconf

--- curtocircuito/urls.py
+++ curtocircuito/urls.py
@@ -1,10 +1,9 @@
 from django.conf.urls import patterns, include, url
-from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 
 admin.autodiscover()
 
-urlpatterns = i18n_patterns('',
+urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('cms.urls')),
 )
