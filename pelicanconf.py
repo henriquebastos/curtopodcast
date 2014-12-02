7336e3fdbe370c3ef6776eae66111f9a4686a26e
Henrique Bastos <henrique@bastos.net>
2014-12-02 02:36:29 -0200

    Configure markdown

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
+from pyembed.markdown import PyEmbedMarkdown
+from markdown.extensions.nl2br import Nl2BrExtension
+
 
 AUTHOR = 'Henrique Bastos'
 SITENAME = 'Curto Circuito Podcast'
@@ -51,4 +54,34 @@ PAGE_SAVE_AS = '{slug}/index.html'
 THEME = "themes/cc"
 
 ITUNES_URL = 'https://itunes.apple.com/us/podcast/curto-circuito-podcast/id712723389'
+
+
+from markdown.inlinepatterns import Pattern
+from markdown import Extension
+
+class AudioPattern(Pattern):
+    PATTERN = '\[!audio(\?(?P<attr>.*))?\]\((?P<url>.*)\)'
+
+    def __init__(self, md):
+        super(AudioPattern, self).__init__(self.PATTERN)
+
+        self.md = md
+
+    def handleMatch(self, m):
+        params = m.groupdict()
+        print(params)
+        html = '<audio controls=""><source src="{url}" {attr}">Seu browser não suporta a tag de audio HTML5.</audio>'.format(**params)
+
+        return self.md.htmlStash.store(html)
+
+class AudioMarkdown(Extension):
+    def __init__(self, renderer=None):
+        super(AudioMarkdown, self).__init__()
+        self.renderer = renderer
+
+    def extendMarkdown(self, md, md_globals):
+        md.inlinePatterns.add(
+            'audio', AudioPattern(md), '_begin')
+
+MD_EXTENSIONS = [PyEmbedMarkdown(), Nl2BrExtension(), AudioMarkdown(), 'markdown.extensions.def_list', 'markdown.extensions.sane_lists']
 
