e2ccf8b6f393f93468c3798482647db6adceba5f
Henrique Bastos <henrique@bastos.net>
2014-12-03 16:26:26 -0200

    Inject project root into Python Path

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
+from __future__ import print_function
 from __future__ import unicode_literals
+import sys
+import os
+# hack
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+
 from jinja_filters import rfc2822
 
 
