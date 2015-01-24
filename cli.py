20698c09885ed550a18f37d509f768c6639ec9ae
Henrique Bastos <henrique@bastos.net>
2015-01-24 14:12:11 -0200

    Update episode’s ID3

--- cli.py
+++ cli.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python
 # coding: utf-8
+from __future__ import unicode_literals
 import click
+import eyed3
+from eyed3.id3 import ID3_V2_3
 from livereload import Server, shell
+import re
+from unipath import Path
+
 from code.config import ROOT, OUTPUT, CONTENT, EXTENSIONS
 from code.entry import next_entry, entry_factory
 
@@ -12,7 +18,7 @@ def cli():
 
 
 @cli.command()
-@click.option('port', default=5500, type=int, help='wsgi server port')
+@click.argument('port', default=5500, type=int)
 def server(port=5500, output=OUTPUT, content=CONTENT, extensions=EXTENSIONS):
     shell('make clean')
     shell('make html')
@@ -66,6 +72,52 @@ def entry(title, episode, audio):
         click.echo('No audio defined.')
 
 
+@cli.command()
+@click.argument('audio', type=click.Path(exists=True, dir_okay=False, resolve_path=True))
+@click.option('-e', '--episode', type=int)
+@click.option('-t', '--title', required=True)
+@click.option('-i', '--image', type=click.Path(exists=True, dir_okay=False, resolve_path=True), help='Image filename for cover art')
+def id3(audio, episode, title, image):
+    """
+    Update ID3 tags with episode info.
+    """
+
+    if not episode:  # Try to detect episode from audio filename
+        match = re.search(r'e(\d{2,})', audio)
+        if not match:
+            raise click.UsageError('Could not be detected episode. Use --episode option.')
+        episode = int(match.group(1))
+
+    id3 = eyed3.load(audio)
+
+    id3.initTag(version=ID3_V2_3)
+
+    id3.tag.title = title
+    id3.tag.artist = 'Henrique Bastos'
+    id3.tag.album = 'Curto Circuito Podcast'
+    id3.tag.track_num = episode
+    id3.tag.genre = 'Podcast'
+
+    if image:
+        image = Path(image)
+        data = image.read_file('rb')
+        mime = 'image/' + image.ext[1:]
+
+        # HACK to make APIC header readable on iTunes
+        # EYED3 sets v2.3 encoding to UTF-16, but iTunes only reads LATIN-1
+        from eyed3.id3 import frames
+        def force_latin1(self):
+            self.encoding = eyed3.id3.LATIN1_ENCODING
+        setattr(frames.ImageFrame, '_initEncoding', force_latin1)
+
+        # force mime as str because eyeD3 uses it to compose the binary header
+        # PUBLISHER_LOGO == 0x14
+        id3.tag.images.set(0x14, data, str(mime))
+
+    id3.tag.save(version=ID3_V2_3)
+
+    # Print id3
+    shell(['eyeD3', audio])
 
 
 if __name__ == '__main__':
